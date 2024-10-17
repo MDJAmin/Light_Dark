c3e841a92d3707d80222af36b882795bbe879a32
MDJAmin <mohammadamindf@gmail.com>
2024-10-17 17:01:18 +0330

    Feat : Complete the functionality and styles

--- src/Components/Toggle/index.jsx
+++ src/Components/Toggle/index.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import "./Toggle.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons";
 
-export default function index() {
+export const Toggle = ({ handleChange, isChecked }) => {
   return (
-    <div>index</div>
-  )
-}
+    <div className="toggle-container">
+      <label htmlFor="check" className="toggle-label">
+        <input
+          type="checkbox"
+          id="check"
+          className="toggle"
+          onChange={handleChange}
+          checked={isChecked}
+        />
+        <span className="slider">
+          <FontAwesomeIcon
+            icon={faSun}
+            className={`icon sun ${isChecked ? "hidden" : ""}`}
+          />
+          <FontAwesomeIcon
+            icon={faMoon}
+            className={`icon moon ${isChecked ? "" : "hidden"}`}
+          />
+        </span>
+      </label>
+    </div>
+  );
+};
+                              
